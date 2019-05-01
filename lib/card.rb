1d7ca618bdc18afc3618e02eee09c7385ff01f64
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-04-30 18:36:40 -0600

    Delete unecessary code

--- lib/card.rb
+++ lib/card.rb
@@ -1,11 +1,9 @@
 class Card
 
-  attr_accessor :question, :answer, :category
+  attr_reader :question, :answer, :category
   def initialize(question, answer, category)
       @question = question
       @answer = answer
       @category = category
   end
 end
-
-card.new = ("What is the capital of Alaska?", "Juneau", :Geography)
