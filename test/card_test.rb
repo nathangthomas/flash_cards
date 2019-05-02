e8d04888411034020b32e8863cb470e834576697
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-05-01 23:10:23 -0600

    Adjust require

--- test/card_test.rb
+++ test/card_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/turn'
+require './lib/deck'
 require 'pry'
 
 class CardTest < Minitest::Test
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,8 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/turn'
 require './lib/card'
-require 'pry'
+require './lib/turn'
+require './lib/deck'
+require 'pry''
+
 
 card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 turn = Turn.new("Juneau", card)
